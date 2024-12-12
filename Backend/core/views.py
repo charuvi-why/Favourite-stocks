b008398029ef15d6c17d6ab23ecfce8fd3be4004
Charuvi <charuvipandey01@gmail.com>
2024-12-13 01:29:18 +0530

    xyz

---
+++ Backend/core/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.generics import ListAPIView, ListCreateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import *
+from .serialiser import *
+
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Hello, World!")
+
+class FavoriteStocksListView(ListAPIView):
+    serializer_class = FavoriteStockModelSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = FavoriteStockModel.objects.all().filter(user=self.request.user)
+
+        return queryset
+    
+class FavoriteStockCreateView(ListCreateAPIView):
+    serializer_class = FavoriteStockModelSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=self.request.user)
+        serializer.save()
+        return HttpResponse("Stock added to favorites")
+
+    
+    def get_queryset(self):
+        queryset = FavoriteStockModel.objects.all().filter(user=self.request.user)
+        return queryset
+    
+
+class FavoriteStockDeleteView(DestroyAPIView):
+        serializer_class = FavoriteStockModelSerializer
+        permission_classes = []
+
+        def get_object(self):
+             uuid = self.kwargs.get('uuid')
+             try:
+                return FavoriteStockModel.objects.get(uuid=uuid)
+
+             except FavoriteStockModel.DoesNotExist:
+                 return HttpResponse("Favorite Stock not found")
