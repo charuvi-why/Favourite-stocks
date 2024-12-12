5f9eb3c3763e5ad5f3e2f21f8740d60002bc52b4
Charuvi <charuvipandey01@gmail.com>
2024-12-13 01:23:10 +0530

    xyz

---
+++ Backend/Accounts/migrations/alter_user_user.py
@@ -0,0 +1,17 @@
+import accounts.models
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='username',
+            field=models.CharField(default=accounts.models.generate_username, max_length=100, null=True, unique=True),
+        ),
+    ]
